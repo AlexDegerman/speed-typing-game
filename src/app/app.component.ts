c4c0bff3267736cd78b01232082696c6f4ed0886
AlexDegerman <alex.degerman.dev@gmail.com>
2022-07-12 01:12:45 +0300

    add character checker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,41 @@ export class AppComponent {
   quoteDisplayElement!: ElementRef;
   @ViewChild('quoteInput')
   quoteInputElement!: ElementRef;
-
   constructor(private randomApi: RandomQuoteApiService) {}
   ngOnInit(): void {
-    this.getQuote()
+    this.getQuote();
   }
   getQuote() {
     this.randomApi.getRandomQuote().subscribe((data: Quote) => {
-      this.quoteDisplayElement.nativeElement.innerHTML = data.content
-    })
+      this.quoteDisplayElement.nativeElement.innerText=""
+      this.quote = data.content;
+      this.quote.split('').forEach((character: string) => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        this.quoteDisplayElement.nativeElement.appendChild(characterSpan);
+      });
+      this.quoteInputElement.nativeElement.value = null;
+    });
+  }
+  checkLetter(event: any) {
+    const arrayQuote = this.quoteDisplayElement.nativeElement.querySelectorAll('span');
+    const arrayValue = this.quoteInputElement.nativeElement.value.split('');
+    let correct = true;
+    arrayQuote.forEach((characterSpan: any, index: any) => {
+      const character = arrayValue[index];
+      if (character == null) {
+        characterSpan.classList.remove('correct');
+        characterSpan.classList.remove('incorrect');
+        correct = false;
+      } else if (character === characterSpan.innerText) {
+        characterSpan.classList.add('correct');
+        characterSpan.classList.remove('incorrect');
+      } else {
+        characterSpan.classList.remove('correct');
+        characterSpan.classList.add('incorrect');
+        correct = false;
+      }
+    });
+    if (correct) this.getQuote();
   }
 }
