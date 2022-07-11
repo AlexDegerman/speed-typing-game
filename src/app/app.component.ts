01303a585da8aa4c6c9d154e2ad0a0dfba9e16ac
AlexDegerman <alex.degerman.dev@gmail.com>
2022-07-12 00:30:34 +0300

    add random quote to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RandomQuoteApiService } from './random-quote-api.service';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'speed-type-game';
+  quote: any;
+  @ViewChild('quoteDisplay')
+  quoteDisplayElement!: ElementRef;
+  @ViewChild('quoteInput')
+  quoteInputElement!: ElementRef;
+
+  constructor(private randomApi: RandomQuoteApiService) {}
+  ngOnInit(): void {
+    this.getQuote()
+  }
+  getQuote() {
+    this.randomApi.getRandomQuote().subscribe((data: Quote) => {
+      this.quoteDisplayElement.nativeElement.innerHTML = data.content
+    })
+  }
 }
